62e3503cd76a5cf3776ed63c833ad4e0c646af11
529124368 <529124368@qq.com>
2022-10-23 15:11:07 +0900

    add

---
+++ tools.java
@@ -0,0 +1,20 @@
+class Tools {
+    private String name;
+    private Integer age;
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
