861b065dcd1bc762588b2127f90f2b5440597001
529124368 <529124368@qq.com>
2022-12-13 22:56:24 +0900

    add path parmas get

--- demo/src/main/java/com/example/demo/controller/StudentController.java
+++ demo/src/main/java/com/example/demo/controller/StudentController.java
@@ -2,11 +2,14 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.StudentModel;
@@ -20,8 +23,13 @@ public class StudentController {
     @Autowired
     private Environment env;
 
-    @GetMapping("/test")
-    public List<StudentModel> helloWorld() {
+    @GetMapping("/test/{name}/{age}")
+    public List<StudentModel> helloWorld(@PathVariable("name") String name, @PathVariable("age") Integer age,
+            HttpServletRequest request) {
+        String n = request.getParameter("name");
+        System.out.println(n);
+        System.out.println("name => " + name);
+        System.out.println("age is " + age);
         System.out.println(env.getProperty("server.port"));
         logger.error("api be called");
         return studentMapper.search();
