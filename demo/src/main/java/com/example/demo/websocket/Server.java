e9195f59cc17b61dba19b7b2f302c8c7679f0b25
529124368 <529124368@qq.com>
2022-12-16 01:02:05 +0900

    netty add

--- demo/src/main/java/com/example/demo/websocket/Server.java
+++ demo/src/main/java/com/example/demo/websocket/Server.java
@@ -1,16 +1,19 @@
 package com.example.demo.websocket;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Server implements ApplicationListener<ContextRefreshedEvent> {
+    @Autowired
+    SocketServer boot;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         if (event.getApplicationContext().getParent() == null) {
-            System.out.print("系统启动结束");
+            boot.start();
         }
     }
 
